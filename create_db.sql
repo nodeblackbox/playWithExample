d8f41f33043e45259c943468701aa64388e51a1c
anasnasseur <65771915+nodeblackbox@users.noreply.github.com>
2022-10-29 05:50:10 +0100

    small update to the register
     update to the register page makeing a back to home button still not finished but all so adding a glass effect to the back of the register inputs

--- create_db.sql
+++ create_db.sql
@@ -8,4 +8,14 @@ GRANT ALL PRIVILEGES ON myBookshop.* TO 'appuser'@'localhost';
 
 
 CREATE TABLE users (id INT AUTO_INCREMENT, username VARCHAR(50),firstname VARCHAR(50), lastname VARCHAR(50), email VARCHAR(50), hashedPassword VARCHAR(100), PRIMARY KEY(id));
+
+
+CREATE TABLE users (id INT AUTO_INCREMENT, username VARCHAR(20) NOT NULL UNIQUE, 
+											firstname VARCHAR(50) NOT NULL,
+                                            lastname VARCHAR(50) NOT NULL, 
+                                            email VARCHAR(50) NOT NULL UNIQUE, 
+                                            hashedPassword VARCHAR(100) NOT NULL, 
+											PRIMARY KEY(id));
+
+
 u-- CREATE TABLE users (id INT PRIMARY KEY(id) AUTO_INCREMENT,email VARCHAR(50) unsigned,password VARCHAR(64) NOT NULL,username VARCHAR(20) NOT NULL UNIQUE);
