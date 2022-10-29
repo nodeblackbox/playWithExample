4666f58e0e2e9368e28f6f9c9fc9cf375a626e35
anasnasseur <65771915+nodeblackbox@users.noreply.github.com>
2022-10-29 08:31:36 +0100

    up date ready for global css change
    to do
    make every page the same divs as register
    fade-in
    nav section
    glass back ground

--- public/extraScripts/titleSwitcher.js
+++ public/extraScripts/titleSwitcher.js
@@ -3,11 +3,11 @@ const book = "📘";
 setInterval(() => {
   var switcher = 1;
   if (!document.title.startsWith(book)) {
-    document.title = book + document.title + switcher;
-    switcher * -1;
+    document.title = book + document.title;
+    // switcher * -1;
   } else {
-    ocument.title = document.title.replace(book, switcher);
-    switcher * -1;
+    document.title = document.title.replace(book, "");
+    // switcher * -1;
   }
   // if ((switcher = 1)) {
   //   console.log("switcher is 1");
